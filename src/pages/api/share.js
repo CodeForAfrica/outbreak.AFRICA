d43fc799d4952edf921689bc5ca4a37058674a7c
Clemence Kyara <clemence@codeforafrica.org>
2020-03-27 08:22:23 +0300

    Add share api endpoint

---
+++ src/pages/api/share.js
@@ -0,0 +1,45 @@
+import AWS from 'aws-sdk';
+
+export default (req, res) => {
+  const { id, dataUrl } = req.body;
+  if (id && dataUrl) {
+    const ext = dataUrl.split(';')[0].split('/')[1];
+    const Body = Buffer.from(
+      dataUrl.replace(/^data:image\/\w+;base64,/, ''),
+      'base64'
+    );
+
+    // Use S3 ManagedUpload class as it supports multipart uploads
+    AWS.config.update({
+      accessKeyId: process.env.HURUMAP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.HURUMAP_AWS_SECRET_ACCESS_KEY,
+      region: process.env.HURUMAP_AWS_REGION
+    });
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: process.env.HURUMAP_AWS_S3_BUCKET,
+        Key: `media/images/${id}.${ext}`,
+        Body,
+        ContentEncoding: 'base64',
+        ContentType: `image/${ext}`,
+        ACL: 'public-read'
+      }
+    });
+    const promise = upload.promise();
+    return promise.then(
+      data => {
+        res.statusCode = 200;
+        res.end('');
+        return res.statusCode;
+      },
+      data => {
+        res.statusCode = 500;
+        res.end('');
+        return res.statusCode;
+      }
+    );
+  }
+  res.statusCode = 400;
+  res.end('');
+  return res.statusCode;
+};
