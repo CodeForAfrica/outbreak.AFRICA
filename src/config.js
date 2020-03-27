eea40c8b8a0d1d5b8c6bbd94897e001a6504106d
Clemence Kyara <clemence@codeforafrica.org>
2020-03-27 08:22:07 +0300

    Update S3 url

--- src/config.js
+++ src/config.js
@@ -89,7 +89,7 @@ Disallow:
   description:
     'Data driven analysis on development policies, programmes & outcomes in 10 African countries.',
   media: {
-    imageUrl: 'https://takwimu.s3.eu-west-1.amazonaws.com/media/images',
+    imageUrl: 'https://cfa-outbreakafrica.s3-eu-west-1.amazonaws.com/media/images',
     imageType: '.png'
   },
   populationTables: [
