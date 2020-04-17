e43884407d6f38e3be98aeaa67591679e2d4149f
Clemence Kyara <clemence@codeforafrica.org>
2020-04-17 17:10:59 +0300

    Fix leaflet.css issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 
-module.exports = withCSS(withImages());
+module.exports = withCSS(
+  withImages({
+    webpack(config) {
+      // support font & image imports inside css files
+      config.module.rules.push({
+        // leaflet.css loads these images which breaks default webpack
+        test: /(layers|layers-2x|marker-icon)\.png$/,
+        use: [
+          {
+            loader: require.resolve('url-loader')
+          }
+        ]
+      });
+
+      return config;
+    }
+  })
+);
