252a00e0849cdfcf92e762017a2e9064056d1602
Clemence Kyara <clemence@codeforafrica.org>
2020-04-20 12:57:29 +0300

    Add FeaturedResearchers

---
+++ src/components/FeaturedStories/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { StoryList } from '@commons-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {},
+  stories: {}
+}));
+
+function FeatureStories({ stories, ...props }) {
+  const classes = useStyles(props);
+
+  return (
+    <StoryList
+      description="View and explore how we visualise Kenya’s budget data to show how much money each county has received from the national government, and how the money is allocated and utilized based on each county’spriorities"
+      md={3.3}
+      spacing={0}
+      stories={stories}
+      title="Featured Stories"
+      xs={1.3}
+      classes={{ root: classes.root, stories: classes.stories }}
+    />
+  );
+}
+
+export default FeatureStories;
