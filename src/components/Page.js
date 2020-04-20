ecb178dd14cee56b2a658a19b9ef68a91cd48c4e
Clemence Kyara <clemence@codeforafrica.org>
2020-04-20 12:58:03 +0300

    Show Featured components on homepage

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import config from 'config';
 
 import Footer from './Footer';
 import Navigation from './Navigation';
 import SEO from './SEO';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    overflowX: 'hidden'
+  }
+}));
+
 function Page({ children, ...props }) {
+  const classes = useStyles(props);
   return (
-    <>
+    <div className={classes.root}>
       <SEO {...props} />
       <Navigation takwimu={config} />
       {children}
       <Footer takwimu={config} />
-    </>
+    </div>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,72 @@
 import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import FeaturedResearchers from 'components/FeaturedResearchers';
+import FeaturedStories from 'components/FeaturedStories';
 import Page from 'components/Page';
 
-function Home() {
-  return <Page />;
+import { getProfiles, fromTimestamp, useStories } from 'lib';
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  featuredResearchers: {
+    margin: '0 auto',
+    padding: '3.8125rem 1.125rem 0 1.125rem',
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      padding: '3.8125rem 0 0',
+      width: `${(1640 / 1920) * 100}%`
+    }
+  },
+  featuredResearchersProfiles: {
+    width: 'calc(((100vw - 100%) / 2) + 100%)'
+  },
+  featuredStories: {
+    margin: '0 auto',
+    padding: '3.8125rem 1.125rem 0 1.125rem',
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      padding: '3.8125rem 0 0',
+      width: `${(1640 / 1920) * 100}%`
+    }
+  },
+  featuredStoriesStories: {
+    width: 'calc(((100vw - 100%) / 2) + 100%)'
+  }
+}));
+
+function Home(props) {
+  const classes = useStyles(props);
+  const profiles = getProfiles();
+  const foundStories = useStories(
+    'https://pesacheck.org/tagged/public-finance'
+  );
+
+  const stories = foundStories.map(story => ({
+    ...story,
+    createdAt: fromTimestamp(story.createdAt),
+    image: {
+      url: `https://cdn-images-1.medium.com/max/480/${story.virtuals.previewImage.imageId}`
+    }
+  }));
+
+  return (
+    <Page>
+      <div className={classes.featuredResearchers}>
+        <FeaturedResearchers
+          profiles={profiles}
+          classes={{ profiles: classes.featuredResearchersProfiles }}
+        />
+      </div>
+      <div className={classes.featuredStories}>
+        <FeaturedStories
+          stories={stories}
+          classes={{ stories: classes.featuredStoriesStories }}
+        />
+      </div>
+    </Page>
+  );
 }
 
 export default Home;
