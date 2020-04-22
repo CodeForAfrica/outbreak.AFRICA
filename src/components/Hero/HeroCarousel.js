a7425e0afddfb4bea5c8972faa2fee5f34cb2e34
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-04-22 11:24:55 +0300

    css updates

--- src/components/Hero/HeroCarousel.js
+++ src/components/Hero/HeroCarousel.js
@@ -1,6 +1,6 @@
 import Carousel from "react-multi-carousel";
 import CarouselCard from './CarouselCard';
-//import "react-multi-carousel/lib/styles.css";
+import "react-multi-carousel/lib/styles.css";
 
 import carousel1 from "../../assets/images/carusel-1.png";
 import carousel2 from "../../assets/images/carusel-2.png";
