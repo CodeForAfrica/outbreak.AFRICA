285b0436b3273be6d781d7a44edd84cfb7fa0df7
nerdlet <brndnyokabi@gmail.com>
2020-04-22 15:49:06 +0300

    Add divider to join us component

--- src/components/Partners/JoinUs.js
+++ src/components/Partners/JoinUs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography , Divider} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme=> ({
@@ -17,12 +17,28 @@ const useStyles = makeStyles(theme=> ({
     color: 'white'
   },
   link:{
-    textDecoration:'underline'
+    fontSize:'1rem',
+    fontWeight:700
   },
   title:{
     fontSize:'3rem',
     fontWeight:700,
     padding:'1rem 0rem'
+  },
+  divider:{
+    backgroundColor: '#170F49',
+    height:'2px',
+    width:'20%',
+    [theme.breakpoints.up('md')]: {
+      width:'50%'
+    }
+  },
+  learnMore:{
+    paddingTop:'1rem',
+    [theme.breakpoints.up('md')]: {
+      paddingTop:'0.5rem'
+    }
+
   }
 }));
 
@@ -31,9 +47,13 @@ function JoinUs() {
   return (
       <div className={classes.root}>
         <div className={classes.typogrid}>
-        <Typography variant="h2" className={classes.title}>Join us</Typography>
-        <Typography variant="body1" style={{fontSize:'1rem'}}>Be part of the initiative</Typography>
-        <Typography variant="h6" className={classes.link}>LEARN MORE</Typography>
+          <Typography variant="h2" className={classes.title}>Join us</Typography>
+          <Typography variant="body1" style={{fontSize:'1rem'}}>Be part of the initiative</Typography>
+          <div className={classes.learnMore}>
+            <Typography variant="h6" className={classes.link}>LEARN MORE</Typography>
+            <Divider className={classes.divider}/>
+          </div>
+  
         </div>
       </div>
   )
