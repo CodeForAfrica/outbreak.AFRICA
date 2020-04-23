bc68b5d2d11433d193d665b7303be9c5fd7f06c8
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-04-23 17:18:06 +0300

    fix A import

--- src/components/Next/index.js
+++ src/components/Next/index.js
@@ -4,7 +4,7 @@ import { PropTypes } from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-import A from '@commons-ui/core/A';
+import { A } from '@commons-ui/core';
 
 import Link from 'components/Link';
 import Section from 'components/Section';
