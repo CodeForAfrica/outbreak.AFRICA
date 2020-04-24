0740b0ea459f71a66e9fd8a4ddfd266579b7ef89
Clemence Kyara <clemence@codeforafrica.org>
2020-04-24 16:11:18 +0300

    Use @commons-ui/core/Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-// import { Footer } from '@commons-ui/core';
+import { Footer } from '@commons-ui/core';
 
 import pulitzer from 'assets/pulitzer.png';
 import cfaLogo from 'assets/cfa.png';
 
-import Email from 'assets/email.svg';
-import Facebook from 'assets/facebook.svg';
-import Medium from 'assets/group-3.svg';
-import LinkedIn from 'assets/group-3-copy.svg';
-import Twitter from 'assets/twitter.svg';
-
-import Footer from './Footer';
+// import Email from 'assets/Icon awesome-at.svg';
+import Facebook from 'assets/Icon awesome-facebook-f.svg';
+import GitHub from 'assets/Icon awesome-git.svg';
+import Instagram from 'assets/Icon awesome-instagram.svg';
+import LinkedIn from 'assets/Icon awesome-linkedin-in.svg';
+import Twitter from 'assets/Icon awesome-twitter.svg';
+import cc from 'assets/cc.svg';
 
 const QUICK_LINKS = [
   {
@@ -32,6 +32,11 @@ const QUICK_LINKS = [
   },
 ];
 
+const LEGAL_LINKS = [
+  { href: '#', label: 'PRIVACY POLICY' },
+  { href: '#', label: 'TERMS OF SERVICE' },
+];
+
 const ABOUT = {
   about:
     'Lorem ipsum dolor sit amet, consetetur sadipscing elitr,\n' +
@@ -43,26 +48,47 @@ const ABOUT = {
     'This initiative was made possible with support \nfrom Pulitzer Center.',
 };
 
+const COPYRIGHT_LOGO = {
+  image: cc,
+  alt: 'Creative Commons',
+  link: '#',
+};
+
 const INITIATIVE_LOGO = {
   image: pulitzer,
   alt: 'Plutizer Center',
   link: 'https://link.url',
 };
 
-const CFA = {
+const ORGANIZATION_LOGO = {
   image: cfaLogo,
   alt: 'CodeForAfrica',
+  link: '#',
 };
 
 const SOCIAL_MEDIA = {
-  support: {
-    email: 'hello@contact.com',
-    image: {
-      url: Email,
-      alt: 'Email',
-    },
-  },
+  // support: {
+  //   email: 'hello@contact.com',
+  //   image: {
+  //     url: Email,
+  //     alt: 'Email',
+  //   },
+  // },
   socialMedia: [
+    {
+      url: 'https://linkedin.com',
+      image: {
+        url: LinkedIn,
+        alt: 'LinkedIn',
+      },
+    },
+    {
+      url: 'https://instagram.com',
+      image: {
+        url: Instagram,
+        alt: 'Instagram',
+      },
+    },
     {
       url: 'https://twitter.com',
       image: {
@@ -78,17 +104,10 @@ const SOCIAL_MEDIA = {
       },
     },
     {
-      url: 'https://medium.com',
+      url: 'https://github.com/CodeForAfrica',
       image: {
-        url: Medium,
-        alt: 'Medium',
-      },
-    },
-    {
-      url: 'https://linkedin.com',
-      image: {
-        url: LinkedIn,
-        alt: 'LinkedIn',
+        url: GitHub,
+        alt: 'GitHub',
       },
     },
   ],
@@ -99,10 +118,12 @@ function MainFooter(props) {
     <Footer
       {...props}
       about={SOCIAL_MEDIA}
-      quickLinks={QUICK_LINKS}
       aboutSection={ABOUT}
+      copyrightLogo={COPYRIGHT_LOGO}
       initiativeLogo={INITIATIVE_LOGO}
-      CFA={CFA}
+      legalLinks={LEGAL_LINKS}
+      quickLinks={QUICK_LINKS}
+      organizationLogo={ORGANIZATION_LOGO}
     />
   );
 }
