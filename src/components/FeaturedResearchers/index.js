38688dadf670a0a476193dcc87dda15c4dd12f91
Clemence Kyara <clemence@codeforafrica.org>
2020-04-27 17:32:30 +0300

    Use upgraded commons-ui to render list items

--- src/components/FeaturedResearchers/index.js
+++ src/components/FeaturedResearchers/index.js
@@ -1,47 +1,68 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { ProfileList } from "@commons-ui/core";
 
-const useStyles = makeStyles(() => ({
-  root: {},
-  profiles: {},
+import { ProfileList, RichTypography, Section } from "@commons-ui/core";
+
+import "simplebar/dist/simplebar.min.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    overflow: "visible",
+  },
+  section: {},
+  profileListProfile: {
+    "&.profile-0:after": {
+      backgroundColor: theme.palette.primary.main,
+    },
+    "&.profile-1:after": {
+      backgroundColor: `#000000`,
+    },
+    "&.profile-2:after": {
+      backgroundColor: theme.palette.highlight.main,
+    },
+  },
+  profileListProfiles: {},
 }));
 
 function FeatureResearchers({ description, profiles, title, ...props }) {
   const classes = useStyles(props);
 
   return (
-    <Grid className={classes.root}>
-      <Grid
-        item
-        xs={12}
-        container
-        className={classes.heading}
-        justify="flex-start"
-        alignItems="center"
-      >
+    <div className={classes.root}>
+      <Section classes={{ root: classes.section }}>
+        <Grid
+          container
+          className={classes.heading}
+          justify="flex-start"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <RichTypography variant="h2" className={classes.title}>
+              {title || "Featured Researchers"}
+            </RichTypography>
+          </Grid>
+          <Grid item>
+            <RichTypography variant="subtitle1" className={classes.description}>
+              {description ||
+                "Connect with African scientists and other experts who are at the forefront of efforts to understand coronavirus on the continent."}
+            </RichTypography>
+          </Grid>
+        </Grid>
         <Grid item xs={12}>
-          <Typography variant="h2" className={classes.title}>
-            {title || "Featured Researchers"}
-          </Typography>
+          <ProfileList
+            profiles={profiles}
+            classes={{
+              root: classes.profileList,
+              profile: classes.profileListProfile,
+              profiles: classes.profileListProfiles,
+            }}
+          />
         </Grid>
-        <Grid item>
-          <Typography variant="body1" className={classes.description}>
-            {description ||
-              "Connect with African scientists and other experts who are at the forefront of efforts to understand coronavirus on the continent."}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item xs={12}>
-        <ProfileList
-          profiles={profiles}
-          classes={{ root: classes.root, profiles: classes.profiles }}
-        />
-      </Grid>
-    </Grid>
+      </Section>
+    </div>
   );
 }
 
--- src/components/FeaturedStories/index.js
+++ src/components/FeaturedStories/index.js
@@ -1,27 +1,68 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { StoryList } from "@commons-ui/core";
 
-const useStyles = makeStyles(() => ({
-  root: {},
-  stories: {},
+import { RichTypography, StoryList, Section } from "@commons-ui/core";
+
+import "simplebar/dist/simplebar.min.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    overflow: "visible",
+  },
+  section: {},
+  profileListProfile: {
+    "& .profile-0:after": {
+      backgroundColor: theme.palette.primary.main,
+    },
+    "& .profile-1:after": {
+      backgroundColor: `#000000`,
+    },
+    "& .profile-2:after": {
+      backgroundColor: theme.palette.highlight.main,
+    },
+  },
+  profileListProfiles: {},
 }));
 
-function FeatureStories({ stories, ...props }) {
+function FeatureStories({ description, stories, title, ...props }) {
   const classes = useStyles(props);
 
   return (
-    <StoryList
-      description="View and explore how we visualise Kenya’s budget data to show how much money each county has received from the national government, and how the money is allocated and utilized based on each county’s priorities"
-      md={3.3}
-      spacing={0}
-      stories={stories}
-      title="Featured Stories"
-      xs={1.3}
-      classes={{ root: classes.root, stories: classes.stories }}
-    />
+    <div className={classes.root}>
+      <Section classes={{ root: classes.section }}>
+        <Grid
+          container
+          className={classes.heading}
+          justify="flex-start"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <RichTypography variant="h2" className={classes.title}>
+              {title || "Featured Stories"}
+            </RichTypography>
+          </Grid>
+          <Grid item>
+            <RichTypography variant="subtitle1" className={classes.description}>
+              {description ||
+                "View and explore how we visualise Kenya’s budget data to show how much money each county has received from the national government, and how the money is allocated and utilized based on each county’s priorities"}
+            </RichTypography>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <StoryList
+            stories={stories}
+            classes={{
+              root: classes.storyList,
+              story: classes.storyListStory,
+              stories: classes.storyListStories,
+            }}
+          />
+        </Grid>
+      </Section>
+    </div>
   );
 }
 
