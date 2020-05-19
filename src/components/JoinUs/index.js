cab0ee0188b23798cb67db591ef130e746f08e10
nerdlet <brndnyokabi@gmail.com>
2020-05-19 15:56:23 +0300

    Add join us content

--- src/components/JoinUs/index.js
+++ src/components/JoinUs/index.js
@@ -2,34 +2,35 @@ import React from 'react'
 
 import { Grid, Typography, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-
-import LinkButton from 'components/Link/Button';
-import img1 from 'assets/joinus-illo-1.svg'
-import img2 from 'assets/joinus-illo-2.svg'
+import img2 from 'assets/joinus-illo-1.svg'
+import img1 from 'assets/joinus-illo-2.svg'
 
 const useStyles = makeStyles({
   root: {
     width: "100%",
     margin: '3rem 0rem',
+    padding: "1rem 0rem",
     backgroundColor: '#f9ff71'
   },
   section: {},
+  container: {},
   contentContainer: {
     dlexGrow: 1,
     display: 'flex',
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "flex-start",
     alignItems: 'center',
   },
-  img2: {
-    height: "439.75px",
-    width: '343.77px'
+  img1: {
+    height: 'auto',
+    width: '100%',
+    maxHeight: '200px'
   },
   img2: {
-    height: "518.58px",
-    width: '459.24px'
+    height: 'auto',
+    width: '100%',
+    maxHeight: '300px'
   }
-
 });
 
 function JoinUs() {
@@ -44,8 +45,9 @@ function JoinUs() {
           alignItems="center"
           className={classes.container}>
           <Grid item xs={7} className={classes.contentContainer}>
-            <img src={img2} alt="img2" className={classes.img2} />
-
+            <div>
+              <img src={img1} alt="img2" className={classes.img1} />
+            </div>
             <Grid item>
               <Typography variant="h4">Join us</Typography>
               <Typography variant="body1">Be part of the movement</Typography>
@@ -53,7 +55,9 @@ function JoinUs() {
             </Grid>
           </Grid>
           <Grid item xs={5}>
-            <img src={img1} alt="img1" className={classes.img1} />
+            <div>
+              <img src={img2} alt="img1" className={classes.img2} />
+            </div>
           </Grid>
         </Grid>
       </div>
