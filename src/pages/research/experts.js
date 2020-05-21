b5da132dadf0a2fe61a1766c0891994fad4ac271
Clemence Kyara <clemence@codeforafrica.org>
2020-05-21 16:35:03 +0300

    Organize pages and pull data from CMS

--- src/pages/research/experts.js
+++ src/pages/research/experts.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Page from "components/Page";
 import Hero from "components/Hero";
+import Subscribe from "components/Subscribe";
+import ProfileList from "components/Research/ProfileList";
 
 import config from "config";
 import { getSitePage } from "cms";
+import { getProfiles } from "lib";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -23,13 +25,26 @@ const useStyles = makeStyles((theme) => ({
       width: "102.5rem",
     },
   },
+  featuredExperts: {
+    marginTop: "3.5rem",
+    [theme.breakpoints.up("md")]: {
+      marginTop: "3.8125rem",
+    },
+  },
+  subscribe: {
+    marginTop: "3.5rem",
+    [theme.breakpoints.up("md")]: {
+      marginTop: "3.8125rem",
+    },
+  },
 }));
 
-function FeaturedExperts({ outbreak, ...props }) {
+function Research({ outbreak, ...props }) {
   const classes = useStyles(props);
   const {
     page: { hero_carousel: heroCarousel },
   } = outbreak;
+  const profiles = getProfiles();
 
   return (
     <Page outbreak={outbreak} classes={{ section: classes.section }}>
@@ -37,12 +52,24 @@ function FeaturedExperts({ outbreak, ...props }) {
         heroCarousel={heroCarousel}
         classes={{ section: classes.section }}
       />
-      <Grid classes={{ section: classes.section }}>Featured experts</Grid>
+      <ProfileList
+        profiles={profiles}
+        classes={{
+          root: classes.featuredExperts,
+          section: classes.section,
+        }}
+      />
+      <Subscribe
+        classes={{
+          root: classes.subscribe,
+          section: classes.section,
+        }}
+      />
     </Page>
   );
 }
 
-FeaturedExperts.getInitialProps = async (props) => {
+Research.getInitialProps = async (props) => {
   const {
     query: { lang: pageLanguage },
   } = props;
@@ -54,4 +81,4 @@ FeaturedExperts.getInitialProps = async (props) => {
   };
 };
 
-export default FeaturedExperts;
+export default Research;
--- src/pages/research/index.js
+++ src/pages/research/index.js
@@ -1,84 +1,23 @@
-import React from "react";
+import { useRouter } from "next/router";
 
-import { makeStyles } from "@material-ui/core/styles";
+const DEFAULT_RESEARCH_PAGE = "/research/experts";
 
-import Page from "components/Page";
-import Hero from "components/Hero";
-import Subscribe from "components/Subscribe";
-import ProfileList from "components/Research/ProfileList";
-
-import config from "config";
-import { getSitePage } from "cms";
-import { getProfiles } from "lib";
-
-const useStyles = makeStyles((theme) => ({
-  root: {},
-  section: {
-    margin: "0 1.25rem 0 1.375rem",
-    width: "auto",
-    [theme.breakpoints.up("lg")]: {
-      margin: "0 auto",
-      width: "78.5rem",
-    },
-    [theme.breakpoints.up("xl")]: {
-      margin: "0 auto",
-      width: "102.5rem",
-    },
-  },
-  featuredExperts: {
-    marginTop: "3.5rem",
-    [theme.breakpoints.up("md")]: {
-      marginTop: "3.8125rem",
-    },
-  },
-  subscribe: {
-    marginTop: "3.5rem",
-    [theme.breakpoints.up("md")]: {
-      marginTop: "3.8125rem",
-    },
-  },
-}));
-
-function Research({ outbreak, ...props }) {
-  const classes = useStyles(props);
-  const {
-    page: { hero_carousel: heroCarousel },
-  } = outbreak;
-  const profiles = getProfiles();
-
-  return (
-    <Page outbreak={outbreak} classes={{ section: classes.section }}>
-      <Hero
-        heroCarousel={heroCarousel}
-        classes={{ section: classes.section }}
-      />
-      <ProfileList
-        profiles={profiles}
-        classes={{
-          root: classes.featuredExperts,
-          section: classes.section,
-        }}
-      />
-      <Subscribe
-        classes={{
-          root: classes.subscribe,
-          section: classes.section,
-        }}
-      />
-    </Page>
-  );
+function Research() {
+  const router = useRouter();
+  // Make sure we're in the browser
+  if (typeof window !== "undefined") {
+    router.push(DEFAULT_RESEARCH_PAGE);
+  }
 }
 
-Research.getInitialProps = async (props) => {
-  const {
-    query: { lang: pageLanguage },
-  } = props;
-  const lang = pageLanguage || config.DEFAULT_LANG;
-  const outbreak = await getSitePage("index", lang);
-
+export async function getServerSideProps(context) {
+  // Check we're on the server via context.res
+  if (context.res) {
+    context.res.writeHead(302, { Location: DEFAULT_RESEARCH_PAGE });
+    context.res.end();
+  }
   return {
-    outbreak,
+    props: {},
   };
-};
-
+}
 export default Research;
