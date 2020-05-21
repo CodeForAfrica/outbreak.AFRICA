82d327f91eb99e1d9450d24e9716c59d0cd51bcb
Clemence Kyara <clemence@codeforafrica.org>
2020-05-21 09:04:14 +0300

    Add ability to show icon on section titles

--- src/components/ProfileSection/Title.js
+++ src/components/ProfileSection/Title.js
@@ -1,28 +1,71 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import ContentLoader from "@hurumap-ui/core/ContentLoader";
+import { RichTypography } from "@commons-ui/core";
+
+import demographicIcon from "assets/icon-demographic.svg";
+import economicIcon from "assets/icon-economic.svg";
+import epidemiologicalIcon from "assets/icon-epidemiological.svg";
+import healthsystemIcon from "assets/icon-healthsystem.svg";
+import infrastructureIcon from "assets/icon-infrastructure.svg";
+import mobilityIcon from "assets/icon-mobility.svg";
+import safetyIcon from "assets/icon-safety.svg";
+import socioEconomicIcon from "assets/icon-socioeconomic.svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: "3rem",
     width: "100%",
   },
-  title: {
-    margin: "1.5rem 3rem 1.5rem 0",
-  },
   grow: {
     flexGrow: 1,
     height: 2,
     backgroundColor: theme.palette.secondary.main,
   },
+  icon: {
+    marginRight: "1rem",
+  },
+  title: {
+    display: "flex",
+    alignItems: "center",
+    margin: "1.5rem 3rem 1.5rem 0",
+  },
 }));
+
+const ICONS = {
+  demographic: demographicIcon,
+  economic: economicIcon,
+  epidemiological: epidemiologicalIcon,
+  healthsystem: healthsystemIcon,
+  infrastructure: infrastructureIcon,
+  mobility: mobilityIcon,
+  safety: safetyIcon,
+  socialeconomic: socioEconomicIcon,
+};
+
+function slugifyName(name) {
+  switch (name.toLowerCase()) {
+    case "epidemiological factors":
+      return "epidemiological";
+    case "healthcare system factors":
+      return "healthsystem";
+    case "demographic vulnerability":
+      return "demographic";
+    case "wash / basic services":
+      return "socialeconomic";
+    default:
+      return undefined;
+  }
+}
 
 function ProfileSectionTitle({ loading, tab: { name }, ...props }) {
   const classes = useStyles(props);
+  const slug = slugifyName(name);
+  const iconSrc = slug && ICONS[slug];
 
   return (
     <Grid item xs={12} container alignItems="center" className={classes.root}>
@@ -32,9 +75,12 @@ function ProfileSectionTitle({ loading, tab: { name }, ...props }) {
         </ContentLoader>
       ) : (
         <>
-          <Typography variant="h3" className={classes.title}>
+          <RichTypography variant="h3" className={classes.title}>
+            {iconSrc && (
+              <img src={iconSrc} alt="icon" className={classes.icon} />
+            )}
             {name}
-          </Typography>
+          </RichTypography>
           <div className={classes.grow} />
         </>
       )}
