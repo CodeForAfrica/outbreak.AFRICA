4fa637f869aeb64015d80fccd613245aa09c0690
Clemence Kyara <clemence@codeforafrica.org>
2020-05-25 09:29:31 +0300

    Hook to compute partialVisibilityGutter
     + Uses current breakpoint & window size (width specifically)

---
+++ src/components/Hero/usePartialVisibilityGutter.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+import { useTheme } from "@material-ui/core";
+
+import useWindowSize from "lib/useWindowSize";
+import useBreakpoint from "lib/useBreakpoint";
+
+function usePartialVisibilityGutter(spacing = 0) {
+  const theme = useTheme();
+  const breakpoint = useBreakpoint();
+  const { width: windowWidth } = useWindowSize();
+  const [partialVisibilityGutter, setPartialVisibilityGutter] = useState(0);
+  useEffect(() => {
+    if (["md", "lg", "xl"].includes(breakpoint)) {
+      const pageWidth = theme.widths.values[breakpoint];
+      const gutter = (windowWidth - pageWidth) / 2 - spacing;
+      setPartialVisibilityGutter(gutter);
+    } else {
+      setPartialVisibilityGutter(0);
+    }
+  }, [breakpoint, windowWidth]);
+  return partialVisibilityGutter;
+}
+
+export default usePartialVisibilityGutter;
