f58724687e8492fd593f121a9f7618792fa0e460
Clemence Kyara <clemence@codeforafrica.org>
2020-05-25 09:32:54 +0300

    Switch to partialVisible & adjustable gutter & size
     + Use width/height props to compute Carousel size,
     + use usePartialVisibilityGutter to compute visibility size of next
     item

--- src/components/Hero/Carousel.js
+++ src/components/Hero/Carousel.js
@@ -1,123 +1,176 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-import { Grid, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
-import Carousel from "react-multi-carousel";
+import ReactMultiCarousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
+import useBreakpoint from "lib/useBreakpoint";
+
+import ButtonGroup from "./CarouselButtonGroup";
 import CarouselCard from "./CarouselCard";
+import usePartialVisibilityGutter from "./usePartialVisibilityGutter";
 
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 1.5,
+const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
+  root: {
+    position: "relative",
   },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 1.5,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1.05,
-  },
-};
-
-const useStyles = makeStyles((theme) => ({
-  buttonContainer: {
+  buttonGroup: {
     display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
+    [breakpoints.up("md")]: {
+      display: "initial",
       position: "absolute",
-      top: "50px",
+      left: (props) =>
+        typography.pxToRem(
+          (widths.values.md * props.width) / widths.values.xl -
+            (48 * 2 + 16) -
+            29
+        ),
+      top: typography.pxToRem(29),
+    },
+    [breakpoints.up("lg")]: {
+      left: (props) =>
+        typography.pxToRem(
+          (widths.values.lg * props.width) / widths.values.xl -
+            (48 * 2 + 16) -
+            29
+        ),
+    },
+    [breakpoints.up("xl")]: {
+      left: (props) => typography.pxToRem(props.width - (70 * 2 + 16) - 29),
     },
   },
-  buttonGrid: {
-    marginLeft: "1rem",
+  buttonGroupButton: {
+    [breakpoints.up("xl")]: {
+      height: typography.pxToRem(70),
+      width: typography.pxToRem(70),
+    },
   },
-  button: {
-    backgroundColor: "#fafafa",
-    boxShadow: "0px 3px 6px #00000029",
+  carousel: {
+    [breakpoints.up("md")]: {
+      width: (props) =>
+        (widths.values.md * props.width) / widths.values.xl +
+        props.partialVisibilityGutter,
+    },
+    [breakpoints.up("lg")]: {
+      width: (props) =>
+        (widths.values.lg * props.width) / widths.values.xl +
+        props.partialVisibilityGutter,
+    },
+    [breakpoints.up("xl")]: {
+      width: (props) => props.width + props.partialVisibilityGutter,
+    },
   },
-  carousel: {},
 }));
 
-function CustomArrowButtons({ next, previous }) {
-  const classes = useStyles();
-  return (
-    <Grid container justify="center" className={classes.buttonContainer}>
-      <Grid item className={classes.buttonGrid}>
-        <IconButton
-          aria-label="back"
-          color="secondary"
-          className={classes.button}
-          onClick={() => previous()}
-        >
-          <ArrowBackIcon />
-        </IconButton>
-      </Grid>
-      <Grid item className={classes.buttonGrid}>
-        <IconButton
-          aria-label="forward"
-          color="secondary"
-          className={classes.button}
-          onClick={() => next()}
-        >
-          <ArrowForwardIcon />
-        </IconButton>
-      </Grid>
-    </Grid>
-  );
-}
+function Carousel({ deviceType, carouselItems, carouselLinkTitle, ...props }) {
+  const partialVisibilityGutter = usePartialVisibilityGutter();
+  const classes = useStyles({ ...props, partialVisibilityGutter });
+  const [centerMode, setCenterMode] = useState(true);
+  const [partialVisible, setPartialVisible] = useState(false);
+  const [responsive, setResponsive] = useState({
+    xl: {
+      breakpoint: { max: 3000, min: 1920 },
+      items: 1,
+    },
+    lg: {
+      breakpoint: { max: 1919, min: 1280 },
+      items: 1,
+    },
+    md: {
+      breakpoint: { max: 1279, min: 960 },
+      items: 1,
+    },
+    sm: {
+      breakpoint: { max: 959, min: 600 },
+      items: 1,
+    },
+    xs: {
+      breakpoint: { max: 599, min: 0 },
+      items: 1,
+    },
+  });
+  const breakpoint = useBreakpoint();
+  useEffect(() => {
+    if (partialVisibilityGutter) {
+      setResponsive((prevResponsive) => ({
+        ...prevResponsive,
+        [breakpoint]: {
+          ...prevResponsive[breakpoint],
+          partialVisibilityGutter,
+        },
+      }));
+      setCenterMode(false);
+      setPartialVisible(true);
+    } else {
+      setCenterMode(true);
+      setPartialVisible(false);
+    }
+  }, [breakpoint, partialVisibilityGutter]);
 
-function HeroCarousel({ deviceType, carouselItems, carouselLinkTitle }) {
   if (!carouselItems || carouselItems.length === 0) {
     return null;
   }
+  const { height, width } = props;
   return (
-    <Carousel
-      swipeable
-      draggable={false}
-      showDots={false}
-      responsive={responsive}
-      ssr // means to render carousel on server-side.
-      infinite
-      autoPlay={false}
-      keyBoardControl
-      renderButtonGroupOutside
-      customButtonGroup={<CustomArrowButtons />}
-      autoPlaySpeed={5000}
-      deviceType={deviceType}
-      removeArrowOnDeviceType={["desktop", "tablet", "mobile"]}
-      dotListClass="custom-dot-list-style"
-      itemClass="carousel-item-padding-40-px"
-    >
-      {carouselItems.map((item) => (
-        <CarouselCard
-          key={item.title}
-          item={item}
-          linkTitle={carouselLinkTitle}
-        />
-      ))}
-    </Carousel>
+    <div className={classes.root}>
+      <ReactMultiCarousel
+        centerMode={centerMode}
+        swipeable
+        draggable={false}
+        showDots={false}
+        responsive={responsive}
+        ssr // means to render carousel on server-side.
+        infinite
+        autoPlay={false}
+        keyBoardControl
+        renderButtonGroupOutside
+        customButtonGroup={
+          <ButtonGroup
+            classes={{
+              root: classes.buttonGroup,
+              button: classes.buttonGroupButton,
+            }}
+          />
+        }
+        autoPlaySpeed={5000}
+        deviceType={deviceType || breakpoint}
+        partialVisible={partialVisible}
+        removeArrowOnDeviceType={["xl", "lg", "md", "sm", "xs"]}
+        itemClass="carousel-item"
+        className={classes.carousel}
+      >
+        {carouselItems.map((item) => (
+          <CarouselCard
+            key={item.title}
+            height={height}
+            item={item}
+            linkTitle={carouselLinkTitle}
+            width={width}
+          />
+        ))}
+      </ReactMultiCarousel>
+    </div>
   );
 }
 
-HeroCarousel.propTypes = {
+Carousel.propTypes = {
   carouselItems: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string,
     })
   ),
   carouselLinkTitle: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
 };
 
-HeroCarousel.defaultProps = {
+Carousel.defaultProps = {
   carouselItems: undefined,
   carouselLinkTitle: undefined,
+  height: 637,
+  width: 536,
 };
 
-export default HeroCarousel;
+export default Carousel;
