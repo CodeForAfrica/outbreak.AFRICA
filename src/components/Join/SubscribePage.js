3ef035231dde5b1c505788a0b6371de39a57d1af
nerdlet <brndnyokabi@gmail.com>
2020-05-27 22:59:59 +0300

    Add subscribe page

---
+++ src/components/Join/SubscribePage.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import classNames from "classnames";
+
+import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Section } from "@commons-ui/core";
+
+import Form from 'components/Join/Form';
+import Subscribe from 'components/Partners/Subscribe'
+
+const useStyles = makeStyles(({ breakpoints }) => ({
+  containerGrid: {
+    padding: '3rem 0rem'
+  },
+  subscribeGrid: {
+    backgroundColor: '#170F49',
+    height: 'auto',
+    padding: '4rem',
+    width: '100%',
+    [breakpoints.up("md")]: {
+      padding: '1.5rem',
+      height: '614px',
+    }
+  },
+  contentGrid: {
+    padding: "1rem 0rem",
+    marginLeft: '5rem'
+  },
+  formGrid: {
+    width: '100%'
+  }
+}));
+
+function Content({ title, subtitle }) {
+  const classes = useStyles();
+  return (
+    <div className={classes.contentGrid}>
+      <Typography variant="h2">{title}</Typography>
+      <Typography variant="body1">
+        {subtitle}
+      </Typography>
+    </div>
+  )
+}
+
+function SubscribePage() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Section classes={{ root: classes.section }}>
+        <Content
+          title="Subscribe"
+          subtitle="Lorem Ipsum is simply dummy text of the printing 
+        and typesetting industry. It was popularised in the 1960s 
+        with the release of Letraset sheets containing Lorem Ipsum"
+        />
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          className={classes.containerGrid}>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            className={classes.subscribeGrid}>
+            <Subscribe />
+          </Grid>
+          <Grid item xs={12} md={8} className={classes.formGrid}>
+            <Form />
+          </Grid>
+        </Grid>
+      </Section>
+    </div>
+  )
+}
+
+export default SubscribePage
---
+++ src/pages/subscribe.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { makeStyles } from "@material-ui/core/styles";
+import config from "config";
+import { getSitePage } from "cms";
+
+import Page from "components/Page";
+import SubscribePage from "components/Join/SubscribePage";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  section: {
+    margin: "0 1.25rem 0 1.375rem",
+    width: "auto",
+    [theme.breakpoints.up("lg")]: {
+      margin: "0 auto",
+      width: "78.5rem",
+    },
+    [theme.breakpoints.up("xl")]: {
+      margin: "0 auto",
+      width: "102.5rem",
+    },
+  },
+  subscribe: {
+    marginTop: "3.5rem",
+    [theme.breakpoints.up("md")]: {
+      marginTop: "3.8125rem",
+    },
+  },
+}));
+
+function JoinUs({ outbreak, ...props }) {
+  const classes = useStyles(props);
+
+  const {
+    page: { },
+  } = outbreak;
+
+  return (
+    <Page outbreak={outbreak} classes={{ section: classes.section }}>
+      <SubscribePage
+        classes={{
+          root: classes.subscribe,
+          section: classes.section
+        }}
+      />
+    </Page>
+  );
+}
+
+JoinUs.propTypes = {
+  outbreak: PropTypes.shape({
+    language: PropTypes.string,
+    page: PropTypes.shape({
+      rendered: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+JoinUs.getInitialProps = async (props) => {
+  const {
+    query: { lang: pageLanguage },
+  } = props;
+  const lang = pageLanguage || config.DEFAULT_LANG;
+  const {
+    page: { featured_experts: featuredExperts },
+  } = await getSitePage("research-experts", lang);
+  const outbreak = await getSitePage("index", lang);
+
+  return {
+    outbreak,
+    featuredExperts,
+  };
+};
+
+export default JoinUs;
