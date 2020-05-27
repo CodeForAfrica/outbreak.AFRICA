66dd2649de66804053761b122d257abec50e7ed1
nerdlet <brndnyokabi@gmail.com>
2020-05-27 21:32:50 +0300

    Add form input boxes

--- src/components/Join/Form.js
+++ src/components/Join/Form.js
@@ -1,43 +1,82 @@
 import React from 'react';
-import { Grid, TextField, Typography } from "@material-ui/core";
-import Input from '@material-ui/core/Input';
-import FilledInput from '@material-ui/core/FilledInput';
-import InputLabel from '@material-ui/core/InputLabel';
+import {
+  Grid,
+  Typography,
+  FilledInput,
+  InputLabel,
+  FormControl,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import FormControl from '@material-ui/core/FormControl';
-
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "812px"
+  },
   expansionPanel: {
-    backgroundColor: '#C1D5FF'
+    backgroundColor: '#C1D5FF',
+    padding: '1.5rem'
   },
-  input: {
-    color: 'white',
-    //fontSize: theme.typography.caption.fontSize,
-    //lineHeight: theme.typography.caption.lineHeight,
-    //borderBottom: "1px solid white",
+  title: {
+    fontWeight: 500
   },
+  formGrid: {
+    padding: '3rem 0rem',
+  },
+  inputGrid: {
+    padding: '2rem 0rem'
+  },
+  inputLabel: {
+    color: 'black'
+  },
+  formControl: {
+    backgroundColor: '#FFFFFF;',
+    border: '1px solid #9D9C9C',
+    borderRadius: 10,
+    '&:focus': {
+      backgroundColor: '#fff'
+    }
+  },
+  filledInput: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    '&:focus': {
+      backgroundColor: '#fff'
+    }
+  },
+  focussed: {}
 }));
 
 function Form() {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState('panel1');;
+  const [expanded, setExpanded] = React.useState('panel1');
+  const [name, setName] = React.useState();
+
+  const formHandleChange = (event) => {
+    setName(event.target.value);
+  };
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
   return (
-    <Grid container style={{ width: '812px' }}>
-      <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')} className={classes.expansionPanel} style={{ padding: '1.5rem' }}>
+    <Grid container classNamw={classes.root}>
+      <ExpansionPanel
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+        className={classes.expansionPanel}>
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography variant="h6" style={{ fontWeight: 700 }}>Application Form</Typography>
+          <Typography
+            variant="h6"
+            className={classes.title}>
+            Application Form
+            </Typography>
         </ExpansionPanelSummary>
 
         <ExpansionPanelDetails>
@@ -49,40 +88,152 @@ function Form() {
                 making it over 2000 years old.
               </Typography>
             </Grid>
-            <Grid item style={{ padding: '3rem 0rem' }}>
-              <div>
-                <InputLabel varian="standard" style={{ color: 'black' }}>Field descriptor</InputLabel>
-                <TextField
-                  id="standard-full-width"
-                  style={{ margin: 8, color: 'white' }}
-                  fullWidth
-                  margin="normal"
-                  InputLabelProps={{
-                    shrink: true
-                  }}
-                  InputProps={{ className: classes.input }}
+            <form item className={classes.formGrid}>
+              <div className={classes.inputGrid}>
+                <InputLabel
+                  variant="standard"
+                  className={classes.inputLabel}
+                >
+                  Field descriptor
+                  </InputLabel>
+                <FormControl
                   variant="filled"
-                />
-              </div>
-              <div>
-                <InputLabel varian="standard" style={{ color: 'black' }}>Field descriptor</InputLabel>
-                <TextField
-                  id="standard-full-width"
-                  style={{ margin: 8 }}
                   fullWidth
-                  margin="normal"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  variant="filled"
-                  InputProps={{ className: classes.input }}
-                />
+                  className={classes.formControl}
+                >
+                  <FilledInput
+                    disableUnderline={true}
+                    value={name}
+                    onChange={formHandleChange}
+                    className={classes.filledInput} />
+                </FormControl>
               </div>
-            </Grid>
+
+              <div className={classes.inputGrid}>
+                <InputLabel
+                  variant="standard"
+                  className={classes.inputLabel}
+                >
+                  Field descriptor
+                  </InputLabel>
+                <FormControl
+                  variant="filled"
+                  fullWidth
+                  className={classes.formControl}
+                >
+                  <FilledInput
+                    disableUnderline={true}
+                    value={name}
+                    onChange={formHandleChange}
+                    className={classes.filledInput} />
+                </FormControl>
+              </div>
+
+              <div className={classes.inputGrid}>
+                <InputLabel
+                  variant="standard"
+                  className={classes.inputLabel}
+                >
+                  Field descriptor
+                  </InputLabel>
+                <FormControl
+                  variant="filled"
+                  fullWidth
+                  className={classes.formControl}
+                >
+                  <FilledInput
+                    disableUnderline={true}
+                    value={name}
+                    onChange={formHandleChange}
+                    className={classes.filledInput} />
+                </FormControl>
+              </div>
+
+              <div className={classes.inputGrid}>
+                <InputLabel
+                  variant="standard"
+                  className={classes.inputLabel}
+                >
+                  Field descriptor
+                  </InputLabel>
+                <FormControl
+                  variant="filled"
+                  fullWidth
+                  className={classes.formControl}
+                >
+                  <FilledInput
+                    disableUnderline={true}
+                    value={name}
+                    onChange={formHandleChange}
+                    className={classes.filledInput} />
+                </FormControl>
+              </div>
+
+              <div className={classes.inputGrid}>
+                <InputLabel
+                  variant="standard"
+                  className={classes.inputLabel}
+                >
+                  Field descriptor
+                  </InputLabel>
+                <FormControl
+                  variant="filled"
+                  fullWidth
+                  className={classes.formControl}
+                >
+                  <FilledInput
+                    disableUnderline={true}
+                    value={name}
+                    style={{ height: '500px' }}
+                    onChange={formHandleChange}
+                    className={classes.filledInput} />
+                </FormControl>
+              </div>
+
+              <div className={classes.inputGrid}>
+                <InputLabel
+                  variant="standard"
+                  className={classes.inputLabel}
+                >
+                  Field descriptor
+                  </InputLabel>
+                <FormControl
+                  variant="filled"
+                  fullWidth
+                  className={classes.formControl}
+                >
+                  <FilledInput
+                    disableUnderline={true}
+                    value={name}
+                    onChange={formHandleChange}
+                    className={classes.filledInput} />
+                </FormControl>
+              </div>
+
+              <div className={classes.inputGrid}>
+                <InputLabel
+                  variant="standard"
+                  className={classes.inputLabel}
+                >
+                  Field descriptor
+                  </InputLabel>
+                <FormControl
+                  variant="filled"
+                  fullWidth
+                  className={classes.formControl}
+                >
+                  <FilledInput
+                    disableUnderline={true}
+                    value={name}
+                    onChange={formHandleChange}
+                    className={classes.filledInput} />
+                </FormControl>
+              </div>
+            </form>
           </Grid>
         </ExpansionPanelDetails>
       </ExpansionPanel>
-    </Grid>
+    </Grid >
   )
 }
 
