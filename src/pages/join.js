b539830a9a0e5187b9ef70962d14dcf2934a7e76
nerdlet <brndnyokabi@gmail.com>
2020-05-28 12:40:50 +0300

    Add content in pages

---
+++ src/pages/join.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Join() {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Join
---
+++ src/pages/subscribe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Subscribe() {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Subscribe
