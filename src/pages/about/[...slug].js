31338d03f3fe9087d3960c361a2bc6ae1e903117
Clemence Kyara <clemence@codeforafrica.org>
2020-06-02 13:25:20 +0300

    Implement About page minus Hero

--- src/pages/about/[...slug].js
+++ src/pages/about/[...slug].js
@@ -1,40 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-import Page from "components/Page";
+import Content from "components/Content";
 import Hero from "components/Hero";
+import JoinUs from "components/JoinUs";
+import Page from "components/Page";
 
 import config from "config";
-import { getSitePage } from "cms";
+import { getSitePageWithChildren } from "cms";
 
-const useStyles = makeStyles(({ breakpoints }) => ({
+const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
   root: {},
   section: {
     margin: "0 1.25rem 0 1.375rem",
     width: "auto",
-    [breakpoints.up("lg")]: {
+    [breakpoints.up("md")]: {
       margin: "0 auto",
-      width: "78.5rem",
+      width: widths.values.md,
+    },
+    [breakpoints.up("lg")]: {
+      width: widths.values.lg,
     },
     [breakpoints.up("xl")]: {
-      margin: "0 auto",
-      width: "102.5rem",
+      width: widths.values.xl,
     },
   },
+  content: {},
+  joinUs: {
+    marginTop: 0,
+    [breakpoints.up("md")]: {
+      marginTop: typography.pxToRem(50),
+    },
+  },
+  subscribe: {},
 }));
 
-function About({ outbreak, ...props }) {
+function About({ errorCode, outbreak, slug, ...props }) {
   const classes = useStyles(props);
   const {
     page: {
       hero_carousel: heroCarousel,
+      join_us: joinUs,
+      subscribe,
       title: { rendered: pageTitle },
+      children: childPages,
     },
   } = outbreak;
+  useEffect(() => {
+    if (slug) {
+      const sub = document.getElementById(`#${slug}`);
+      if (sub) {
+        sub.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [slug]);
+  const contents =
+    (childPages &&
+      childPages.map((page) => ({
+        ...page,
+        as: `/about/${page.slug}`,
+        href: "/about/[...slug]",
+        name: page.title.rendered,
+      }))) ||
+    [];
 
   return (
     <Page
+      errorCode={errorCode}
       outbreak={outbreak}
       title={pageTitle || "About us"}
       classes={{ section: classes.section }}
@@ -43,6 +76,22 @@ function About({ outbreak, ...props }) {
         heroCarousel={heroCarousel}
         classes={{ section: classes.section }}
       />
+      <Content
+        contents={contents}
+        subscribe={subscribe}
+        classes={{
+          root: classes.content,
+          section: classes.section,
+        }}
+      />
+      <JoinUs
+        classes={{
+          root: classes.joinUs,
+          section: classes.section,
+        }}
+        title={joinUs.title}
+        subtitle={joinUs.description}
+      />
     </Page>
   );
 }
@@ -50,11 +99,21 @@ function About({ outbreak, ...props }) {
 export async function getServerSideProps({ query }) {
   const { lang: pageLanguage, slug: pageSlug } = query;
   const lang = pageLanguage || config.DEFAULT_LANG;
-  const outbreak = await getSitePage("about", lang);
-  const slug = pageSlug || null;
+  const outbreak = await getSitePageWithChildren("about", lang);
+  const [firstSlug] = pageSlug || [];
+  const slug = (firstSlug && firstSlug.toLowerCase()) || null;
+  let errorCode = null;
+  if (slug) {
+    const index = outbreak.page.children
+      ? outbreak.page.children.findIndex((child) => child.slug === slug)
+      : -1;
+    if (index === -1) {
+      errorCode = 404;
+    }
+  }
 
   return {
-    props: { outbreak, slug },
+    props: { errorCode, outbreak, slug },
   };
 }
 
