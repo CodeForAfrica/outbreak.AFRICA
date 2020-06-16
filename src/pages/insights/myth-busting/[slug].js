b26db0e9298918b9a1fe9d22ccb021d9fdbf70a0
Clemence Kyara <clemence@codeforafrica.org>
2020-06-16 09:02:44 +0300

    Switch to single slug for insights pages

---
+++ src/pages/insights/myth-busting/[slug].js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import ArticlePage from "components/ArticlePage";
+import Page from "components/Page";
+
+import config from "config";
+import { getArticle, getSitePage } from "cms";
+
+const useStyles = makeStyles(({ breakpoints, widths }) => ({
+  root: {},
+  section: {
+    margin: "0 1.25rem 0 1.375rem",
+    width: "auto",
+    [breakpoints.up("md")]: {
+      margin: "0 auto",
+      width: widths.values.md,
+    },
+    [breakpoints.up("lg")]: {
+      width: widths.values.lg,
+    },
+    [breakpoints.up("xl")]: {
+      margin: "0 auto",
+      width: widths.values.xl,
+    },
+  },
+}));
+
+function Analysis({ outbreak, article: { post, author, media }, ...props }) {
+  const classes = useStyles(props);
+  const {
+    page: { subscribe, title },
+  } = outbreak;
+  const pageTitle = title || "Misinformation";
+
+  return (
+    <Page
+      outbreak={outbreak}
+      title={pageTitle}
+      classes={{ section: classes.section }}
+    >
+      <ArticlePage
+        author={author}
+        link={{ href: "/insights/myth-busting", title: pageTitle }}
+        media={media}
+        post={post}
+        subscribe={subscribe}
+        classes={{ section: classes.section }}
+      />
+    </Page>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const { lang: pageLanguage, slug } = query;
+  const lang = pageLanguage || config.DEFAULT_LANG;
+  const outbreak = await getSitePage("insights-myth-busting", lang);
+  const article = await getArticle(slug, lang);
+  const errorCode = article ? null : 404;
+
+  return {
+    props: {
+      article,
+      errorCode,
+      outbreak,
+    },
+  };
+}
+
+export default Analysis;
