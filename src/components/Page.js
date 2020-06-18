0fc6957fd9fa171b13e3a5b535c4d9667ae7e83f
Clemence Kyara <clemence@codeforafrica.org>
2020-06-18 14:56:15 +0300

    Try disabling font-boosting

--- src/components/Page.js
+++ src/components/Page.js
@@ -16,6 +16,8 @@ const useStyles = makeStyles(({ breakpoints, typography }) => ({
     // Seems like you need height defined for AppBar position="sticky" to work
     // see: https://github.com/mui-org/material-ui/issues/16186
     height: "100vh",
+    // font-boosting: https://stackoverflow.com/questions/13430897/how-to-override-font-boosting-in-mobile-chrome#comment61478376_16432702
+    maxHeight: 999999,
     overflowX: "hidden",
   },
   section: {
