ecd4b3c32b8bfcc6f911990c9e23d6922750ea81
Xavier Frankline Odhiambo <49587182+thepsalmist@users.noreply.github.com>
2023-07-03 06:58:32 +0000

    update GA tracking ID

--- src/lib/ga.js
+++ src/lib/ga.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "UA-92541368-6";
+export const GA_TRACKING_ID = process.env.GA_TRACKING_ID || "G-12X35Z55SN";
 
 // https://developers.google.com/analytics/devguides/collection/analyticsjs/pages
 export const pageview = (url) => {
